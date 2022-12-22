261fa801c211198d88c938c1e0019b5e8dc4a21e
VsesLom <115865055+VsesLom@users.noreply.github.com>
2022-12-22 20:46:16 +0300

    Delete ЛР4 directory

--- ЛР4/task1.py
+++
@@ -1,5 +0,0 @@
-# решить с помощью list comprehension и распечатать его
-from pprint import pprint
-
-list_dict = [{"dec": i, "bin": bin(i), "oct": oct(i), "hex": hex(i)} for i in range(16)]
-pprint(list_dict)
